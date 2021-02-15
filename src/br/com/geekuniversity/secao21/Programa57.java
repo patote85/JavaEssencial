d252ca7d4494bda9811f58b1307771b9819d8865
patote85 <r.patote@gmail.com>
2021-02-15 17:30:42 -0300

    primeiro commit

---
+++ src/br/com/geekuniversity/secao21/Programa57.java
@@ -0,0 +1,30 @@
+package br.com.geekuniversity.secao21;
+//Lista Ligada / LinkedList
+
+import br.com.geekuniversity.secao11.Cliente;
+
+/*
+Em um vetor, os elementos estão um do lado do outro, enquanto em uma lista
+ligada, eles estão em lugares diferentes, porém um aposta para o outro
+indicando o próximo elemento. 
+
+[1, 2, 3, ]
+*/
+public class Programa57 {
+
+	public static void main(String[] args) {
+		ListaLigada lista = new ListaLigada();
+		
+		System.out.println(lista);
+		
+		lista.adiciona("Angelina");
+		System.out.println(lista);
+		lista.adiciona("Maria");
+		lista.adiciona("Felicity");
+		System.out.println(lista);
+		lista.removeDoComeco();
+		System.out.println(lista);
+
+	}
+
+}
